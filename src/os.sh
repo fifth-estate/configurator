3a552e0e3d0d76be4ae337fdd8ee3aa865f0f2db
Abhishek Chakravarti <abhishek@taranjali.org>
2021-10-31 10:48:10 +0530

    Define OS detection routines
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

---
+++ src/os.sh
@@ -0,0 +1,109 @@
+export rv
+
+
+cfg_os_kernel() 
+{
+	_kern=$(uname -s)
+
+	if [ "$_kern" = "FreeBSD" ] ; then
+		rv=$_kern
+
+	elif [ "$_kern" = "Linux" ] ; then
+		if [ -f /etc/os-release ] ; then
+			rv=$_kern
+		else
+			cfg_tty_emerg "Outdated Linux kernel: $_kern"
+			exit 1
+		fi
+
+	else
+		cfg_tty_emerg "Unsupported kernel: $_kern"
+	fi
+}
+
+
+cfg_os_distro() 
+{
+	cfg_os_kernel
+
+	if [ "$rv" = "Linux" ] ; then
+		rv=$(grep 'NAME' /etc/os-release				\
+	            | head -n1 							\
+		    | cut -d '=' -f 2 						\
+	            | tr -d '"' 						\
+		    | cut -d ' ' -f 1)
+	fi
+
+	if [ "$rv" != "FreeBSD" ]						\
+	    && [ "$rv" != "Arch" ]						\
+	    && [ "$rv" != "Debian" ] 						\
+	    && [ "$rv" != "Ubuntu" ] ; then	
+		cfg_tty_emerg "Unsupported distribution: $rv"
+		exit 1
+	fi
+}
+
+
+cfg_os_version() 
+{
+	cfg_os_distro
+	_distro=$rv
+
+	if [ "$_distro" = "Arch" ] ; then
+		rv="rolling"
+
+	elif [ "$_distro" = "FreeBSD" ] ; then
+		rv=$(uname -r)
+		
+		_ver=$(echo "$rv"	\
+		    | cut -d '-' -f 1	\
+		    | tr -d '.')
+
+		if [ "$_ver" -lt 122 ] ; then
+			cfg_tty_notice "FreeBSD version $rv detected"
+			cfg_tty_alert "FreeBSD version >= 12.2 required"
+			exit 1
+		fi
+
+	elif [ "$_distro" = "Debian" ] ; then
+		rv=$(grep 'VERSION=' /etc/os-release	\
+		    | tr -d '"' 			\
+		    | cut -d '=' -f 2)
+		
+		_ver=$(echo "$rv" | tr -d 'A-Z a-z ( ) .')
+
+		if [ "$_ver" -lt 10 ] ; then
+			cfg_tty_notice "Debian version $rv detected"
+			cfg_tty_alert "Debian version >= 10 required"
+			exit 1
+		fi
+
+	else
+		rv=$(grep "VERSION_ID" /etc/os-release	\
+		    | tr -d '"'				\
+		    | cut -d '=' -f 2)
+		
+		_ver=$(echo "$rv" | tr -d 'A-Z a-z ( ) .')
+
+		if [ "$_ver" -lt 2004 ] ; then
+			cfg_tty_notice "Ubuntu version $rv detected"
+			cfg_tty_alert "Ubuntu version >= 20.04 required"
+			exit 1
+		fi
+	fi
+}
+
+
+cfg_os_notify()
+{
+	cfg_os_distro
+	
+	if [ "$rv" = "Arch" ] ; then
+		cfg_tty_info "Arch Linux detected"
+	else
+		_distro="$rv"
+		cfg_os_version
+		core_tty_info "$_distro ($rv) detected"
+	fi
+}
+
